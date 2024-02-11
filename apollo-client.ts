b10d4dbb16c67a756c8d0b46d530b9d4a84ac3d7
Jukoss <tusyukv@gmail.com>
2024-02-12 01:36:24 +0200

    remove unused code

--- apollo-client.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-export const apolloClient = new ApolloClient({
-    uri: "http://localhost:8080/v1/graphql",
-    cache: new InMemoryCache(),
-});
